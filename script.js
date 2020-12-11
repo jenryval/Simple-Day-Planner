6524178360ad2ca220af8048ff18e09165908c07
jenry valdes <jenryval93@gmail.com>
2020-12-10 22:59:06 -0600

    added local storage

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ console.log('time', currentTime, typeof(currentTime))
 $("#currentDay").text(currentDay)
 // display current time
 $("#currentTime").text(currentTime)
+
+$('.saveBtn').on('click', function (event) {
+    event.preventDefault()
+    var nine = $('#nine').val();
+    var ten = $('#ten').val();
+    var eleven = $('#eleven').val();
+    var twelve = $('#twelve').val();
+    var one = $('#one').val();
+    var two = $('#two').val();
+    var three = $('#three').val();
+    var four = $('#four').val();
+    var five = $('#five').val();
+
+    localStorage.setItem('9AM', nine);
+    localStorage.setItem('10AM', ten);
+    localStorage.setItem('11AM', eleven);
+    localStorage.setItem('12PM', twelve);
+    localStorage.setItem('1PM', one);
+    localStorage.setItem('2PM', two);
+    localStorage.setItem('3PM', three);
+    localStorage.setItem('4PM', four);
+    localStorage.setItem('5PM', five);
+});
